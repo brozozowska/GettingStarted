dab8056c35ed6a6885047b652df51af6d38c92e3
brozozowska <s_rozov@me.com>
2023-03-19 17:03:00 +0300

    Initial Commit

---
+++ GettingStarted/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Сергей Розов on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+    }
+    
+}
+
